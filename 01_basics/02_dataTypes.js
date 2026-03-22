ae83aebd6b74fc63354aaaf4088a733f74c4b9d6
Amit Pal <palamit9411@gmail.com>
2026-03-22 14:32:45 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS as newer version of JS
+
+//alert(3 + 3) we are using nodejs, not browser
+
+console.log(3 + 3)
+//code readability should be high
+console.log("Sumit")
+
+let name = "Anil"
+let age = 20
+let isloggedin = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => abhi value assign nhi h
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
