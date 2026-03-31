90526dec55f8b5a7c3efbe8ca079f1a0d9e3f150
Amit Pal <palamit9411@gmail.com>
2026-03-31 07:27:26 +0000

    String

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+const name  = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-Coudhary')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8)
+console.log(anotherString);
+
