4a6ecc84ae3e17c11e08c73959c697e1e4208e18
Amit Pal <palamit9411@gmail.com>
2026-04-12 08:57:15 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,21 @@
+// var c = 300
+let a = 100
+if (true) {
+    let a = 10
+    const b = 20
+    // var c = 30
+    // c = 30
+    console.log("INNER: ", a);
+    
+}
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+
+console.log(a)
+// INNER:  10
+// 100
+// console.log(b)
+// console.log(c) 
+// 30
